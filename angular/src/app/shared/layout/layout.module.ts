a9a8fc6df606fbc2a9678ebfd6d0a1df76b9ced1
Martin Emil Dolezel <xdoleze4@mendelu.cz>
2025-04-04 15:59:21 +0200

    changes

---
+++ angular/src/app/shared/layout/layout.module.ts
@@ -0,0 +1,29 @@
+﻿import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {MatButton, MatButtonModule} from '@angular/material/button';
+import {MatDrawer, MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatList, MatListItem} from '@angular/material/list';
+import {MainLayoutComponent} from "./main-layout/main-layout.component";
+import {HeaderComponent} from "./header/header.component";
+import {MenuComponent} from "./menu/menu.component";
+
+@NgModule({
+    declarations: [
+        MainLayoutComponent,
+        HeaderComponent,
+        MenuComponent,
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        MatTooltipModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatListItem,
+        MatList,
+        MatSidenavModule,
+    ]
+})
+export class LayoutModule { }
